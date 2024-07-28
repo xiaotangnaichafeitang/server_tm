9bc1c6fc1391230d2c555824a8865ccbe808b8ff
xiaotangxiaotangnaichafeitang <2766314754@qq.com>
2024-07-28 22:34:56 +0800

    friDay log.h and log.cpp

---
+++ server_tm/log.cc
@@ -0,0 +1,101 @@
+#include <iostream>
+#include "log.h"
+
+namespace server_tm {
+
+    Logger::Logger(const std::string &name)
+            : m_name(name) {
+
+    }
+
+    void Logger::delAppender(LogAppender::ptr appender) {
+        m_appenders.push_back(appender);
+    }
+
+    void Logger::addAppender(LogAppender::ptr appender) {
+        for (auto it = m_appenders.begin();
+             it != m_appenders.end(); ++it) {
+            if (*it == appender) {
+                m_appenders.erase(it);
+                break;
+            }
+        }
+    }
+
+    void Logger::log(LogLevel::Level level, LogEvent::ptr event) {
+        if (level >= m_level) {
+            for (auto &i: m_appenders) {
+                i->log(level, event);
+            }
+        }
+    }
+
+    void Logger::debug(LogEvent::ptr event) {
+        debug(LogLevel::DEBUG, event);
+    }
+
+    void Logger::info(LogEvent::ptr event) {
+        debug(LogLevel::INFO, event);
+    }
+
+    void Logger::warn(LogEvent::ptr event) {
+        debug(LogLevel::WARN, event);
+    }
+
+    void Logger::error(LogEvent::ptr event) {
+        debug(LogLevel::ERROR, event);
+    }
+
+    void Logger::fatal(LogEvent::ptr event) {
+        debug(LogLevel::FATAL, event);
+    }
+
+    FileLogAppender::FileLogAppender(const std::string &filename)
+            : m_filename(filename) {
+
+    }
+
+    void FileLogAppender::log(LogLevel::Level level, LogEvent::ptr event) {
+        if (level >= m_level) {
+            m_filestream << m_formatter.format(event);
+        }
+    }
+
+    bool FileLogAppender::reopen() {
+        if (m_filestream) {
+            m_filestream.close();
+        }
+        m_filestream.open(m_filename);
+        return !!m_filestream;
+    }
+
+
+    void StdoutLogAppender::log(LogLevel::Level level, LogEvent::ptr event) {
+        if (level >= m_level) {
+            std::cout << m_formatter.format(event);
+        }
+    }
+
+    LogFormatter::LogFormatter(const std::string &pattern)
+            : m_pattern(pattern) {
+
+    }
+
+    std::string LogFormatter::format(LogEvent::ptr event) {
+        std::stringstream ss;
+        for (auto &i: m_items) {
+            i->format(ss, event);
+        }
+        return ss.str();
+
+    }
+    // %xxx %xxx{xxx} %%
+    void LogFormatter::init() {
+
+    }
+
+
+}
+
+
+
