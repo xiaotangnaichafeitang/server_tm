f3a682fd0efabb1284f4276f7f9dd501f2b7cb27
xiaotangxiaotangnaichafeitang <2766314754@qq.com>
2024-08-08 15:11:54 +0800

    Resolve bugs that occur during compilation

--- tests/test.cc
+++ tests/test.cc
@@ -2,11 +2,12 @@
 #include "../server_tm/log.h"
 
 int main(int argc, char** argv) {
-	server_tm::Logger::ptr(new server_tm::Logger);
-	Logger->addAppender(server_tm::LogAppender::ptr(new server_tm::StdoutLogAppender));
+	server_tm::Logger::ptr logger(new server_tm::Logger);
+
+	logger->addAppender(server_tm::LogAppender::ptr(new server_tm::StdoutLogAppender));
 	server_tm::LogEvent::ptr event(new server_tm::LogEvent(__FILE__, __LINE__, 0, 1, 2, time(0)));
 
-	Logger->log(server_tm::LogLevel::DEBUG, event);
+	logger->log(server_tm::LogLevel::DEBUG, event);
 	std::cout << "hello world" << std::endl;
 
 	return 0;
